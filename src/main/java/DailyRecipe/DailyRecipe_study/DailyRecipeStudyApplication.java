fe08cc59416285868e8b06f0246a2ad946457c75
JEKKY <137982621+vJEKKYv@users.noreply.github.com>
2024-09-20 21:47:02 +0900

    add: default files

---
+++ src/main/java/DailyRecipe/DailyRecipe_study/DailyRecipeStudyApplication.java
@@ -0,0 +1,13 @@
+package DailyRecipe.DailyRecipe_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyRecipeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyRecipeStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DailyRecipe/DailyRecipe_study/DailyRecipeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package DailyRecipe.DailyRecipe_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyRecipeStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
