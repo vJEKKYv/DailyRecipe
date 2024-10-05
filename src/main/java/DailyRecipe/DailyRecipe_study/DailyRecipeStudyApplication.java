6dc59512eb3d333b7f3025ee0f5956f117d8401e
JEKKY <137982621+vJEKKYv@users.noreply.github.com>
2024-10-05 22:02:36 +0900

    docs: 10/05 체크 커밋

--- src/main/java/DailyRecipe/DailyRecipe_study/DailyRecipeStudyApplication.java
+++ src/main/java/DailyRecipe/DailyRecipe_study/DailyRecipeStudyApplication.java
@@ -13,4 +13,6 @@ public class DailyRecipeStudyApplication {
 	//2024-10-01 RecipeDAO 구성 및 Repository 연결 - DAO 구성 미완. 구조 파악 필요.
 	//2024-10-02 RecipeController, Service 구조 작성. 내부 구현은 아직 안함.
 	//2024-10-03 구현 x 이제 시작 합니다..
+		// 구현 완료. but 재료와 조리법 List<Map<String, Object>> 형식으로 변경 필요
+	//2024-10-05 금일 구현 X. 금일 안에 재료와 조리법 Converter 이용한 구조 변경 구현 예정
 }
